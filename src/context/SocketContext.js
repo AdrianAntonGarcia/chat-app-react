495b99b7b0b92ab0afc200e16cc10026072302bd
adriton <adrtler@gmail.com>
2021-01-09 19:44:59 +0100

    Conectar y desconectar sockets manualmente

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,10 +1,26 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
+import { AuthContext } from '../auth/AuthContext';
 import { useSocket } from '../hooks/useSocket';
 
 export const SocketContex = createContext();
 
 export const SocketProvider = ({ children }) => {
-  const { socket, online } = useSocket('http://localhost:8080');
+  const { socket, online, conectarSocket, desconectarSocket } = useSocket(
+    'http://localhost:8080'
+  );
+
+  const { auth } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (auth.logged) {
+      conectarSocket();
+    }
+  }, [auth, conectarSocket]);
+  useEffect(() => {
+    if (!auth.logged) {
+      desconectarSocket();
+    }
+  }, [auth, desconectarSocket]);
   return (
     <SocketContex.Provider value={{ socket, online }}>
       {children}
