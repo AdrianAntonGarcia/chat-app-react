d8bdb49641aa651427537eaca5291b3473b27c19
adriton <adrtler@gmail.com>
2021-01-10 18:30:28 +0100

    Activar una sala de chat

--- src/context/chat/ChatReducer.js
+++ src/context/chat/ChatReducer.js
@@ -4,6 +4,12 @@ export const chatReducer = (state, action) => {
   switch (action.type) {
     case types.usuariosCargados:
       return { ...state, usuarios: action.payload };
+    case types.activarChat:
+      // Por si clicamos en el mismo
+      if (state.chatActivo === action.payload) {
+        return state;
+      }
+      return { ...state, chatActivo: action.payload, mensajes: [] };
     default:
       return state;
   }
